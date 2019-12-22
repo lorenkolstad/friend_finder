07fbda6e79e2a600ae7ee599af46bd47f14f6772
Loren Kolstad <lorenkolstad@gmail.com>
2019-12-21 23:00:52 -0800

    Commented out some code to try and get the routes to work, but I still can't figure it out.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,12 +7,17 @@ module.exports = function(app) {
     app.get("/api/friends", function(req, res){
         // res.render("index", { friends: friends, layout: 'layout1'});
         // console.log(friends);
-      
-        res.json(friends)
+        friends.getFriends()
+        .then(friends => res.json(friends))
+        .catch(err => res.status(404).json(err));
+
     });
      
      
     app.post("/api/friends", function(req, res){
+        friends.addFriend(req.body)
+        .then(friend => res.json(friend))
+        .catch(err => res.status(404).json(err));
 
         console.log(req.body)
 
