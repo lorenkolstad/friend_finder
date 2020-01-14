e83f8971ee246d2cc6b35980649dc50e9318f7cc
Loren Kolstad <lorenkolstad@gmail.com>
2020-01-13 18:45:57 -0800

    Adding changes. At least got my server listening again, but the paths are still having trouble when I go to the localhost.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,36 +1,26 @@
-// const friends = require("../data/friends.js") 
+const friends = require("../data/friends.js") 
 
-// console.log(friends)
+console.log(friends)
 
-// module.exports = function(app) {
+module.exports = function(app) {
     
-//     app.get("/api/friends", function(req, res){
-//         // res.render("index", { friends: friends, layout: 'layout1'});
-//         console.log(friends);
-//         // friends.getFriends()
-//         // .then(friends => res.json(friends))
-//         // .catch(err => res.status(404).json(err));
-//         friends.Friends.findAll({}).then(function(foundFriends) {
-//             res.json(foundFriends);
-//         });
+    app.get("/api/friends", function(req, res){
+        res.json(friends);
+    });
 
-//     });
-     
-     
-//     app.post("/api/friends", function(req, res){
-//         // friends.addFriend(req.body)
-//         // .then(friend => res.json(friend))
-//         // .catch(err => res.status(404).json(err));
+    // app.post("/api/friends", function(req, res){
+    //     console.log(req.body);
+    //     .then(friend => res.json(friend))
+    //     .catch(err => res.status(404).json(err));
 
-//         console.log(req.body)
+    //     console.log(req.body)
 
-//         friends.Friends.create({
-//             name: req.body.name,
-//             photo: req.body.photo,
-//             scores: req.body.scores
-//         }).then(function(addFriend) {
-//             res.json(addFriend);
-//         });
+    //     friends.Friends.create({
+    //         name: req.body.name,
+    //         photo: req.body.photo,
+    //         scores: req.body.scores
+    //     }).then(function(addFriend) {
+    //         res.json(addFriend);
+    //     });
 
-//     });
-// };
+};
