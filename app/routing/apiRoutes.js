597e1bfc9ab3d8a0629059479a22c992f9c596ba
Loren Kolstad <lorenkolstad@gmail.com>
2020-02-24 14:53:14 -0800

    Got the app to load! I had issues in the htmlRouts.js using a plus sign after _dirname.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,26 +1,61 @@
-const friends = require("../data/friends.js") 
+const friends = require("../data/friends.js")
 
 console.log(friends)
 
-module.exports = function(app) {
-    
-    app.get("/api/friends", function(req, res){
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
 
-    // app.post("/api/friends", function(req, res){
-    //     console.log(req.body);
-    //     .then(friend => res.json(friend))
-    //     .catch(err => res.status(404).json(err));
+    app.post("/api/friends", function (req, res) {
 
-    //     console.log(req.body)
+        let user = req.body;
 
-    //     friends.Friends.create({
-    //         name: req.body.name,
-    //         photo: req.body.photo,
-    //         scores: req.body.scores
-    //     }).then(function(addFriend) {
-    //         res.json(addFriend);
-    //     });
+        console.log(user);
 
+        for (let i = 0; i < user.scores.length; i++) {
+            user.scores[i] = parseInt(user.scores[i]);
+        }
+
+        let bestFriendScoreDifference = 20;
+
+        for (let x = 0; x < friends.length; x++) {
+            let totalDifference = 0;
+            let newBestFriend = 0;
+
+            for (let a = 0; a < friends[x].scores.length; a++) {
+                let difference = Math.abs(user.scores[a] - friends[x].scores[a]);
+
+                totalDifference += difference;
+            }
+
+            if (totalDifference < bestFriendScoreDifference) {
+                newBestFriend = x;
+                bestFriendScoreDifference = totalDifference;
+            }
+        }
+
+        friends.push(user);
+
+        res.json(friends[newBestFriend]);
+
+    });
 };
+
+
+//             .then(friend => res.json(friend))
+//             .catch(err => res.status(404).json(err));
+
+//         console.log(req.body)
+
+//         friends.Friends.create({
+//             name: req.body.name,
+//             photo: req.body.photo,
+//             scores: req.body.scores
+//         }).then(function (addFriend) {
+//             res.json(addFriend);
+//         });
+//     });
+
+// };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,15 +1,20 @@
 const path = require("path");
 
-module.exports = function(app) {
-    app.get("/", function(req,res) {
-        res.sendFile(path.join(__dirname, + "../public/home.html"));
+module.exports = function (app) {
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     })
 
-    app.get("/survey", function(req, res) {
-        res.sendFile(path.join(__dirname, + "../public/survey.html"));
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     })
 
-    app.use(function(req, res) {
-        res.sendFile(path.join(__dirname, + "../public/home.html"));
+    app.get("/home", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
+
+
+    // app.use(function(req, res) {
+    //     res.sendFile(path.join(__dirname, + "../public/home.html"));
+    // });
 }
